48e42345ddb3fe4e852a567fad1174a39d6ab6e5
Dennis Walter <dennis.walter@gmail.com>
2021-09-26 22:58:15 +0200

    WIP Send enum messages.

--- multicast/README.md
+++ multicast/README.md
@@ -1,6 +1,7 @@
 # Cross-compile for Raspberry 2
 
 ```shell
+rustup target add armv7-unknown-linux-gnueabihf
 sudo apt install gcc-arm-linux-gnueabihf
 cargo build --release --target=armv7-unknown-linux-gnueabihf
 ```
--- multicast/src/main.rs
+++ multicast/src/main.rs
@@ -1,4 +1,5 @@
 use clap::{value_t, App, Arg};
+use serde::{Deserialize, Serialize};
 use std::io;
 use std::net::{Ipv4Addr, UdpSocket};
 use std::time::Duration;
@@ -7,9 +8,10 @@ const IPV4_MULTICAST_ADDR: &'static str = "224.0.0.199";
 const IPV4_MULTICAST_PORT: u16 = 10199;
 const BUF_SIZE: usize = 1024;
 
+#[derive(Serialize, Deserialize, PartialEq, Debug)]
 enum Message {
-    Dsco,
-    Helo { ip_addr: Ipv4Addr },
+    Discover,
+    Hello,
 }
 
 fn main() -> io::Result<()> {
@@ -46,13 +48,18 @@ fn main() -> io::Result<()> {
     let socket = UdpSocket::bind((Ipv4Addr::UNSPECIFIED, IPV4_MULTICAST_PORT))?;
     socket.join_multicast_v4(&multicast_addr, &Ipv4Addr::UNSPECIFIED)?;
     socket.set_read_timeout(Some(Duration::from_millis(1000)))?;
-    // Type of buf will be resolved to u8 later on through usage.
+    // Type of buf will be resolved to [u8; BUF_SIZE] later on through usage.
     let mut buf = [0; BUF_SIZE];
-    socket.send_to(&buf, (multicast_addr, IPV4_MULTICAST_PORT))?;
+    let dsco_msg: Vec<u8> =
+        bincode::serialize(&Message::Discover).expect("Cannot serialize Message.");
+    socket.send_to(&dsco_msg, (multicast_addr, IPV4_MULTICAST_PORT))?;
     loop {
         match socket.recv_from(&mut buf) {
             Ok((n_bytes, src_addr)) => {
                 println!("Received {} bytes from {}", n_bytes, src_addr);
+                let reply: Message =
+                    bincode::deserialize(&buf).expect("Could not deserialize message.");
+                println!("Message is: {:?}", reply);
             }
             Err(e) => {
                 if let io::ErrorKind::WouldBlock = e.kind() {
