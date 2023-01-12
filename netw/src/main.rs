f97382a802caf86bc474630f90ee4f740d390b74
Dennis Walter <dennis.walter@gmail.com>
2023-01-12 23:47:07 +0100

    Get rid of BufReader.

--- netw/src/main.rs
+++ netw/src/main.rs
@@ -2,7 +2,7 @@ use clap::Parser;
 use serde::{Deserialize, Serialize};
 use std::time::Instant;
 use std::{
-    io::{prelude::*, BufReader},
+    io::{prelude::*},
     net::{TcpListener, TcpStream},
 };
 
@@ -53,26 +53,21 @@ fn run_client(args: Args) -> std::io::Result<()> {
     let bytes_upload = args.bytes_upload;
     if bytes_download <= 0 && bytes_upload <= 0 {
         println!("Nothing to do.");
-        return Ok(())
+        return Ok(());
     }
-    let mut stream = TcpStream::connect((args.host, args.port))?;
-    let in_stream = stream.try_clone()?;
-    // Send command
-    serde_json::to_writer(
-        &stream,
+    let mut out_stream = TcpStream::connect((args.host, args.port))?;
+    let in_stream = out_stream.try_clone()?;
+    send_command(
         &NetwCommand::MeasureThroughput(MeasureThroughputParams {
             bytes_download: args.bytes_download,
             bytes_upload: args.bytes_upload,
         }),
+        &out_stream,
     )?;
-    let zero: [u8; 1] = [0; 1];
-    stream.write(&zero)?;
-    stream.flush()?;
-    let mut buf_reader = BufReader::new(in_stream);
     if bytes_download > 0 {
         // Download bytes
         let dl_started = Instant::now();
-        consume_bytes(bytes_download, &mut buf_reader)?;
+        consume_bytes(bytes_download, &in_stream)?;
         let dl_elapsed = dl_started.elapsed().as_micros();
         let dl_rate = bytes_download as f64 / dl_elapsed as f64;
         println!("Download completed: {bytes_download} bytes in {dl_elapsed}us ({dl_rate:.3}MB/s)",);
@@ -80,9 +75,9 @@ fn run_client(args: Args) -> std::io::Result<()> {
     if bytes_upload > 0 {
         // Upload bytes
         let up_started = Instant::now();
-        send_bytes(args.bytes_upload, &mut stream)?;
+        send_bytes(args.bytes_upload, &mut out_stream)?;
         // To measure end-to-end throughput, wait for an ACK from the other side that all data has arrived.
-        consume_bytes(ACK_BYTES, &mut buf_reader)?;
+        consume_bytes(ACK_BYTES, &in_stream)?;
         let up_elapsed = up_started.elapsed().as_micros();
         let up_rate = bytes_upload as f64 / up_elapsed as f64;
 
@@ -116,31 +111,42 @@ fn run_server() -> std::io::Result<()> {
 }
 
 fn handle_connection(stream: TcpStream) -> std::io::Result<()> {
-    let mut buf: Vec<u8> = Vec::with_capacity(4096);
-    let in_stream = stream.try_clone()?;
-    let mut buf_reader = BufReader::new(in_stream);
-    buf_reader.read_until(0, &mut buf)?;
-    buf.pop();
-    match serde_json::from_str::<NetwCommand>(std::str::from_utf8(&buf).unwrap()) {
-        Ok(NetwCommand::MeasureThroughput(params)) => {
+    let out_stream = stream.try_clone()?;
+    match recv_command(&stream)? {
+        NetwCommand::MeasureThroughput(params) => {
             println!(
                 "Received MeasureThroughput command with params {:?}",
                 params
             );
-            return measure_throughput(buf_reader, stream, params);
-        }
-        Err(e) => {
-            println!("Couldn't process command: {:?}", e);
-            return Err(std::io::Error::new(
-                std::io::ErrorKind::Other,
-                e.to_string(),
-            ));
+            return measure_throughput(stream, out_stream, params);
         }
     }
 }
 
+fn send_command(cmd: &NetwCommand, mut out_stream: &TcpStream) -> std::io::Result<()> {
+    let cmd_str = serde_json::to_string(&cmd)?;
+    out_stream.write_all(&(cmd_str.as_bytes().len() as u32).to_be_bytes())?;
+    out_stream.write_all(cmd_str.as_bytes())?;
+    out_stream.flush()
+}
+
+fn recv_command(mut in_stream: &TcpStream) -> std::io::Result<NetwCommand> {
+    let mut len_buf = [0 as u8; 4];
+    in_stream.read_exact(&mut len_buf)?;
+    let cmd_len = u32::from_be_bytes(len_buf);
+    let mut buf: Vec<u8> = vec![0; cmd_len as usize];
+    in_stream.read_exact(&mut buf)?;
+    match serde_json::from_str::<NetwCommand>(std::str::from_utf8(&buf).unwrap()) {
+        Ok(x) => Ok(x),
+        Err(e) => Err(std::io::Error::new(
+            std::io::ErrorKind::Other,
+            e.to_string(),
+        )),
+    }
+}
+
 fn measure_throughput(
-    mut buf_reader: BufReader<TcpStream>,
+    mut in_stream: TcpStream,
     mut out_stream: TcpStream,
     params: MeasureThroughputParams,
 ) -> std::io::Result<()> {
@@ -149,13 +155,13 @@ fn measure_throughput(
         send_bytes(params.bytes_download, &mut out_stream)?;
     }
     if params.bytes_upload > 0 {
-        consume_bytes(params.bytes_upload, &mut buf_reader)?;
+        consume_bytes(params.bytes_upload, &mut in_stream)?;
         send_bytes(ACK_BYTES, &mut out_stream)?;
     }
     Ok(())
 }
 
-fn send_bytes(n_bytes: i64, out_stream: &mut TcpStream) -> std::io::Result<()> {
+fn send_bytes(n_bytes: i64, mut out_stream: &TcpStream) -> std::io::Result<()> {
     let mut rem_bytes: i64 = n_bytes;
     const BUF_SIZE: usize = 8192;
     let buf = vec![42; BUF_SIZE];
@@ -168,14 +174,14 @@ fn send_bytes(n_bytes: i64, out_stream: &mut TcpStream) -> std::io::Result<()> {
         let n_written = out_stream.write(&buf[0..n_bytes])?;
         rem_bytes -= n_written as i64;
     }
-    Ok(())
+    out_stream.flush()
 }
 
-fn consume_bytes(n_bytes: i64, buf_reader: &mut BufReader<TcpStream>) -> std::io::Result<()> {
+fn consume_bytes(n_bytes: i64, mut in_stream: &TcpStream) -> std::io::Result<()> {
     let mut buf = [0 as u8; 8192];
     let mut rem_bytes: i64 = n_bytes;
     while rem_bytes > 0 {
-        let n_read = buf_reader.read(&mut buf)?;
+        let n_read = in_stream.read(&mut buf)?;
         rem_bytes -= n_read as i64;
     }
     Ok(())
